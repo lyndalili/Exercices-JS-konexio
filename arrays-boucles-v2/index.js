dc41f88157b4a68fb387dcd74f0b552336bd98d1
lyndalili <lyndahaddad50@gmail.com>
2022-06-22 17:18:19 +0200

    the last commit for the day

---
+++ arrays-boucles-v2/index.js
@@ -0,0 +1,74 @@
+/*let numbr_pair = []
+for (var i = 11; i < 47; i++) {
+    numbr_pair[i] = i * 2
+}
+console.log(numbr_pair.join('-'))
+
+*/
+
+
+
+
+/*
+var number_impair = []
+
+for (var i = 190; i < 588; i++) {
+    number_impair[i] = i * 2 + 1
+}
+console.log(number_impair.join('-'))*/
+
+
+
+
+
+/*
+let multiplication = 3;
+for (let i = 0; i <= 10; i++) {
+    console.log(multiplication + " x " + i + " = " + multiplication * i);
+}
+*/
+
+
+
+/*
+let multiplication = 1;
+for (let i = 0; i <= 10; i++) {
+    console.log(multiplication + " x " + i + " = " + multiplication * i);
+
+    let multiplication2 = 2;
+    for (let i = 0; i <= 10; i++) {
+        console.log(multiplication2 + " x " + i + " = " + multiplication2 * i);
+    }
+    let multiplication3 = 3;
+    for (let i = 0; i <= 10; i++) {
+        console.log(multiplication3 + " x " + i + " = " + multiplication3 * i);
+    }
+    let multiplication4 = 4;
+    for (let i = 0; i <= 10; i++) {
+        console.log(multiplication4 + " x " + i + " = " + multiplication4 * i);
+    }
+    let multiplication5 = 5;
+    for (let i = 0; i <= 10; i++) {
+        console.log(multiplication5 + " x " + i + " = " + multiplication5 * i);
+    }
+    let multiplication6 = 6;
+    for (let i = 0; i <= 10; i++) {
+        console.log(multiplication6 + " x " + i + " = " + multiplication6 * i);
+    }
+    let multiplication7 = 7;
+    for (let i = 0; i <= 10; i++) {
+        console.log(multiplication7 + " x " + i + " = " + multiplication7 * i);
+    }
+    let multiplication8 = 8;
+    for (let i = 0; i <= 10; i++) {
+        console.log(multiplication8 + " x " + i + " = " + multiplication8 * i);
+    }
+    let multiplication9 = 9;
+    for (let i = 0; i <= 10; i++) {
+        console.log(multiplication9 + " x " + i + " = " + multiplication9 * i);
+    }
+    let multiplication10 = 10;
+    for (let i = 0; i <= 10; i++) {
+        console.log(multiplication10 + " x " + i + " = " + multiplication10 * i);
+    }
+}*/
