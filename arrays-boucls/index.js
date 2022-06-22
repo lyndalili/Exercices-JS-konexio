8bdde76b79c73a503d72b9acd2a346edfcfe1b5b
lyndalili <lyndahaddad50@gmail.com>
2022-06-22 14:13:08 +0200

    the third  commit

--- arrays-boucls/index.js
+++ arrays-boucls/index.js
@@ -6,12 +6,83 @@
 //console.log(ingredients[2])
 
 
-const objects = ["pen", "book", "lamp"]
-objects.unshift("chair")
-console.log(objects)
-objects.shift()
-console.log(objects)
-objects.push("laptop")
-console.log(objects)
-objects.pop()
-console.log(objects)
+//const objects = ["pen", "book", "lamp"]
+//objects.unshift("chair")
+//console.log(objects)
+//objects.shift()
+//console.log(objects)
+//objects.push("laptop")
+//console.log(objects)
+//objects.pop()
+//console.log(objects)
+
+
+
+//const numbers = [4, 10, 8, 12, 6]
+//numbers.reverse()
+//console.log(numbers)
+
+
+//let totale = 0
+//const limit = 10
+//for (let i = 0; i < limit; i++) {
+//console.log(i)
+
+//}
+
+
+
+//const sentence = "Hello Konexio !"
+//for (let i = sentence.length - 1; i >= 0; sentence[i--])
+//console.log(sentence[i])
+
+
+//for (var nombre = 1; nombre <= 100; nombre++) {
+// var message = "";
+//if (nombre % 3 === 0) {
+
+//  message += 'Fizz';
+// }
+//if (nombre % 5 === 0) {
+
+// message += 'Buzz';
+//}
+// if (message === "") {
+
+//  message = nombre;
+//}
+//console.log(message);
+//}
+
+
+
+
+
+let i = 1;
+
+while (i <= 100) {
+    if ((i % 3 === 0) && (i % 5 === 0)) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+    i++;
+}
+
+
+
+
+
+/*
+let namCollegues = ["jad", "thomas", "danh", "julien", "clara", "florez", "arthur", "edaly", "rayan", "walid1", "walid2", "aurelien", ".."]
+let CollegusAll = Math.floor(Math.random() * namCollegues.length);
+let collegusAleatoires = namCollegues[CollegusAll]
+    //console.log(collegusAleatoires);
+
+for (let i = 0; i < CollegusAll; i++) {
+    console.log(collegusAleatoires);
+}*/
