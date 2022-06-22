59df65da197584f4d7bddb781786dcdd05827adf
lyndalili <lyndahaddad50@gmail.com>
2022-06-22 14:38:47 +0200

    the third commit

--- arrays-boucls/index.js
+++ arrays-boucls/index.js
@@ -43,16 +43,16 @@ for (let nombre = 1; nombre <= 100; nombre++) {
 
     if (nombre % 3 === 0) {
 
-        console.log("Fizz");
+        console.log("Fizz")
     }
     if (nombre % 5 === 0) {
 
-        console.log("Buzz");
+        console.log("Buzz")
     }
     if ((nombre % 3 === 0) && (nombre % 5 === 0)) {
         console.log("FizzBuzz");
     }
-    console.log(nombre);
+    console.log(nombre)
 }
 */
 
@@ -63,13 +63,13 @@ for (let nombre = 1; nombre <= 100; nombre++) {
 
 while (i <= 100) {
     if ((i % 3 === 0) && (i % 5 === 0)) {
-        console.log("FizzBuzz");
+        console.log("FizzBuzz")
     } else if (i % 3 === 0) {
-        console.log("Fizz");
+        console.log("Fizz")
     } else if (i % 5 === 0) {
-        console.log("Buzz");
+        console.log("Buzz")
     } else {
-        console.log(i);
+        console.log(i)
     }
     i++;
 }
@@ -80,10 +80,15 @@ while (i <= 100) {
 
 /*
 let namCollegues = ["jad", "thomas", "danh", "julien", "clara", "florez", "arthur", "edaly", "rayan", "walid1", "walid2", "aurelien", ".."]
-let CollegusAll = Math.floor(Math.random() * namCollegues.length);
+let CollegusAll = Math.floor(Math.random() * namCollegues.length)
 let collegusAleatoires = namCollegues[CollegusAll]
-    //console.log(collegusAleatoires);
+    //console.log(collegusAleatoires)
 
 for (let i = 0; i < CollegusAll; i++) {
-    console.log(collegusAleatoires);
+    console.log(collegusAleatoires)
 }*/
+
+let arrays = []
+let max = 100
+let min = 0
+let namberAleatoires = Math.floor(Math.random() * (max)) + min
