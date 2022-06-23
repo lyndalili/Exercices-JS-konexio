b779c49a2f2e3d696c995ddd672de067f4f76505
lyndalili <lyndahaddad50@gmail.com>
2022-06-23 16:28:06 +0200

    first commit functions

--- objects-functions/index.js
+++ objects-functions/index.js
@@ -23,17 +23,14 @@ if (cat.isCute === true) {
 
 
 /*
-const chekIfEven = (num) => {
-
+const checkIfEven = (num) =>{
     if (num % 2 == 0) {
-        console.log("even");
-    } else {
-        console.log("odd");
+            console.log("even")
+        } else {
+            console.log("odd")
+        }
     }
-
-}
-num = (2)
-*/
+    checkIfEven(2)*/
 
 
 
@@ -53,3 +50,52 @@ num = (2)
 }
 
 const Compare = sum(5, 5)*/
+
+
+
+
+
+
+
+/*
+const conversionSeconde = (time) => {
+    var reste = time
+    var result = ''
+
+    var nbJours = Math.floor(reste / (3600 * 24))
+    reste -= nbJours * 24 * 3600
+
+    var nbHours = Math.floor(reste / 3600)
+    reste -= nbHours * 3600
+
+    var nbMinutes = Math.floor(reste / 60);
+    reste -= nbMinutes * 60
+
+    var nbSeconds = reste;
+
+    if (nbJours > 0) {
+        result = result + nbJours + 'Jour'
+
+    }
+
+    if (nbHours > 0) {
+        result = result + nbHours + ':'
+
+    }
+
+    if (nbMinutes > 0) {
+        result = result + nbMinutes + ':'
+
+    }
+
+    if (nbSeconds > 0) {
+        result = result + nbSeconds
+
+    }
+    console.log(result)
+    return result;
+
+
+}
+
+conversionSeconde(3700)*/
