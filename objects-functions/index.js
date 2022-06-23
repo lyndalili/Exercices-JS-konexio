088006f0762ff71e078dad499996c1c71dff6cdf
lyndalili <lyndahaddad50@gmail.com>
2022-06-23 15:14:41 +0200

    creat doc objects-fonctions

---
+++ objects-functions/index.js
@@ -0,0 +1,55 @@
+/*let cat = {
+    name: "Garfiled",
+    age: 3,
+    isCute: true,
+
+}
+console.log(cat.age)
+
+
+let cat = {
+    name: "Garfiled",
+    age: 3,
+    isCute: true,
+
+
+
+}
+
+if (cat.isCute === true) {
+    console.log("So Cut !")
+}
+*/
+
+
+/*
+const chekIfEven = (num) => {
+
+    if (num % 2 == 0) {
+        console.log("even");
+    } else {
+        console.log("odd");
+    }
+
+}
+num = (2)
+*/
+
+
+
+/*const sum = (x, y) => {
+
+    if (x > y) {
+        console.log("X is Bigger")
+
+
+    } else if (x < y) {
+        console.log("y is Bigger")
+
+    } else if (x === y) {
+        console.log("both are same ")
+
+    }
+}
+
+const Compare = sum(5, 5)*/
